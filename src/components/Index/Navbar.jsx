48591292c8c0278508f7e8e24a36ebcbd4d9033b
Ealfred1 <alfrederic371@gmail.com>
2024-10-09 00:21:15 +0100

    Layout and configs

--- src/components/Index/Navbar.jsx
+++ src/components/Index/Navbar.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
 	return (
-		<div>
-			
-		</div>
+		<nav className="navbar bg-transparent backdrop-blur-2xl">
+	      <div className="logo-items">
+	        <h2 className="text-2xl"><Link to="/">DataEase</Link></h2>
+	      </div>
+	      
+	      <div className="navbar-content">
+	        <DarkModeToggle />
+	        <Link to="/register" className="text-lg text-gray-500">Sign up</Link>
+	      </div>
+	      
+    	</nav>
 	)
 }
 
