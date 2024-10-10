7621935f63d9e7365e8e6009d3fb23ec1217a12b
Ealfred1 <alfrederic371@gmail.com>
2024-10-10 15:41:50 +0100

    Navbar style and animations

--- src/components/Index/Layout.jsx
+++ src/components/Index/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
 	return (
 		<div className="max-w-[75rem] mx-auto">
 	        <Navbar />
-		        <div className="max-w-7xl mx-auto mt-24 pt-[220rem]">
+		        <div className="max-w-7xl mx-auto mt-24">
 		          <Outlet />
 		        </div>
 	        <Footer />
