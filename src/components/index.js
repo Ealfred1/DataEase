e4f249eb84bad1ffc31a0b765645a15f5470296e
Ealfred1 <alfrederic371@gmail.com>
2024-10-10 16:30:11 +0100

    Hero section innit

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import ScrollToTop from './ScrollToTop'
 import Layout from './Index/Layout'
 import DarkModeToggle from './DarkModeToggle'
+import Hero from './Index/Hero'
 
 export {
 	ScrollToTop,
 	Layout,
-	DarkModeToggle
+	DarkModeToggle,
+	Hero
 }
