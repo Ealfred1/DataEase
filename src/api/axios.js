b7dca79e0247524d384d15a56440464486b56206
Ealfred1 <alfrederic371@gmail.com>
2024-10-12 00:07:00 +0100

    Creating axios instance for api calls

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axios = axios.create({
-  baseURL: 'https://vtu-h5yu.onrender.com/api', // Change this to your base URL
+  baseURL: 'https://vtu-h5yu.onrender.com/api/', // Change this to your base URL
   headers: {
     'Content-Type': 'application/json',
   },
