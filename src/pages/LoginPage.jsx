44df352759e27ed8c4012ca74ac46de749b97682
Ealfred1 <alfrederic371@gmail.com>
2024-10-11 01:00:56 +0100

    Setting up login page with shadn

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import Lottie from 'lottie-react';
+import animationData from '../assets/Animation - 1728603847074.json'
 
 const LoginPage = () => {
+	const animationRef = useRef(null);
+	
 	return (
 		<div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
 	      <div className="flex items-center justify-center py-12">
 	        <div className="mx-auto grid w-[350px] gap-6">
 	          <div className="grid gap-2 text-center">
-	            <h1 className="text-3xl font-bold">Login</h1>
+	            <h1 className="h3 text-[54px] font-bold text-vibrantGreen">Sign In</h1>
 	            <p className="text-balance text-muted-foreground">
 	              Enter your email below to login to your account
 	            </p>
@@ -54,7 +58,7 @@ const LoginPage = () => {
 	        </div>
 	      </div>
 	      <div className="hidden bg-muted lg:block">
-	        <Image
+	        <img
 	          src="/placeholder.svg"
 	          alt="Image"
 	          width="1920"
