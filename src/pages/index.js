c589047644a5dd2206f4283f5be07e4024c1bc2e
Ealfred1 <alfrederic371@gmail.com>
2024-10-12 22:56:44 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import LandingPage from './LandingPage'
 import NotFound from './NotFound'
 import LoginPage from './LoginPage'
 import RegisterPage from './RegisterPage'
+import DashboardPage from './DashboardPage'
 
 export {
 	LandingPage,
 	NotFound,
 	RegisterPage,
-	LoginPage
+	LoginPage,
+	DashboardPage
 }
