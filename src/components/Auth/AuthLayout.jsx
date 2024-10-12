f1031a27c61e25d67fe9010006512e789428e3ba
Ealfred1 <alfrederic371@gmail.com>
2024-10-12 23:48:04 +0100

    layout merging

--- src/components/Auth/AuthLayout.jsx
+++ src/components/Auth/AuthLayout.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 
 const AuthLayout = () => {
-	return (
-		<div>
-			
-		</div>
-	)
+	const [sideBarClose, setSideBarClose] = useState(false)
+  
+	const handleSideBarToggle = () => {
+	  setSideBarClose(!sideBarClose)
+	}
+  return (
+    <>
+      <div>
+        <Navbar handleSideBarToggle={handleSideBarToggle} closed={sideBarClose} />
+        <SideBar sideBarClose={sideBarClose} handleSideBarToggle={handleSideBarToggle} />
+        <div className="max-w-7xl h-screen mt-24 sm:ml-[90px]">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default AuthLayout
