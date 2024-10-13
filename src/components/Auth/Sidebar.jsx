3eef27614701387cda96c46e7077105c79a3d839
Ealfred1 <alfrederic371@gmail.com>
2024-10-13 22:45:23 +0100

    sidebar style changes

--- src/components/Auth/Sidebar.jsx
+++ src/components/Auth/Sidebar.jsx
@@ -4,7 +4,7 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
   
   const location = useLocation()
   const isActive = (path) => {
-    return location.pathname === path ? 'bg-vibrantGreen text-white' : 'hover:text-red-500'
+    return location.pathname === path ? 'bg-vibrantGreen text-white activea' : 'text-white'
   }
 
   const toggle = () => {
@@ -20,11 +20,19 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
           {/* Menu Section */}
           <div className="menu_title font-semibold menu_dashboard"></div> 
           <li className="item">
+            <Link to="/dashboard" className={`nav_link ${isActive('/dashboard')}`} title='Dashboard'>
+              <span className="navlink_icon">
+                <i className="pi pi-th-large"></i>
+              </span>
+              <span className="navlink2">Dashboard</span>
+            </Link>
+          </li>
+          <li className="item">
             <Link to="/fund" className={`nav_link ${isActive('/fund')}`} title='Fund Account'>
               <span className="navlink_icon">
                 <i className="pi pi-wallet"></i>
               </span>
-              <span className="navlink">Fund Account</span>
+              <span className="navlink2">Fund Account</span>
             </Link>
           </li>
           <li className="item">
@@ -32,7 +40,7 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-wifi"></i>
               </span>
-              <span className="navlink">Data Sub</span>
+              <span className="navlink2">Data Sub</span>
             </Link>
           </li>
           <li className="item">
@@ -40,7 +48,7 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-mobile"></i>
               </span>
-              <span className="navlink">Airtime Sub</span>
+              <span className="navlink2">Airtime Sub</span>
             </Link>
           </li>
           <li className="item">
@@ -48,41 +56,41 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-credit-card"></i>
               </span>
-              <span className="navlink">                  <span className="navlink">Cable Sub</span></span>
+              <span className="navlink2">                  <span className="navlink2">Cable Sub</span></span>
             </Link>
           </li>
           <li>
             <Link to="/bills/electricity-sub" className={`nav_link ${isActive('/bills/electricity-sub')}`} title='Electricity Sub'>
               <span className="navlink_icon"><i className="pi pi-bolt"></i></span>
-              <span className="navlink">Electricity Sub</span>
+              <span className="navlink2">Electricity Sub</span>
             </Link>
           </li>
           <li>
             <Link to="/bills/exam-sub" className={`nav_link ${isActive('/bills/exam-sub')}`} title='NECO/WAEC'>
               <span className="navlink_icon"><i className="pi pi-book"></i></span>
-              <span className="navlink">NECO/WAEC</span>
+              <span className="navlink2">NECO/WAEC</span>
             </Link>
           </li>
 
           {/* Admin Section */}
-          <div className="menu_title font-semibold">Admin</div> 
+          <div className="menu_title font-semibold menu_admin"></div> 
           <li className="item">
             <Link to="/admin" className={`nav_link ${isActive('/admin')}`} title='Admin Panel'>
               <span className="navlink_icon">
                 <i className="pi pi-lock"></i>
               </span>
-              <span className="navlink">Admin Panel</span>
+              <span className="navlink2">Admin Panel</span>
             </Link>
           </li>
 
           {/* Transactions Section */}
-          <div className="menu_title font-semibold">Transactions</div>
+          <div className="menu_title font-semibold menu_trans"></div>
           <li className="item">
             <Link to="/transactions/payment" className={`nav_link ${isActive('/transactions/payment')}`} title='Payment'>
               <span className="navlink_icon">
                 <i className="pi pi-dollar"></i>
               </span>
-              <span className="navlink">Payment</span>
+              <span className="navlink2">Payment</span>
             </Link>
           </li>
           <li className="item">
@@ -90,18 +98,18 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-shopping-cart"></i>
               </span>
-              <span className="navlink">Purchase History</span>
+              <span className="navlink2">Purchase History</span>
             </Link>
           </li>
 
           {/* Profile Section */}
-          <div className="menu_title font-semibold">Profile</div>
+          <div className="menu_title font-semibold menu_setting"></div>
           <li className="item">
             <Link to="/profile/bonus" className={`nav_link ${isActive('/profile/bonus')}`} title='Bonus'>
               <span className="navlink_icon">
                 <i className="pi pi-gift"></i>
               </span>
-              <span className="navlink">Bonus</span>
+              <span className="navlink2">Bonus</span>
             </Link>
           </li>
           <li className="item">
@@ -109,7 +117,7 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-users"></i>
               </span>
-              <span className="navlink">Referrals</span>
+              <span className="navlink2">Referrals</span>
             </Link>
           </li>
           <li className="item">
@@ -117,7 +125,7 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-cog"></i>
               </span>
-              <span className="navlink">Settings</span>
+              <span className="navlink2">Settings</span>
             </Link>
           </li>
           <li className="item">
@@ -125,18 +133,18 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-code"></i>
               </span>
-              <span className="navlink">Developers API</span>
+              <span className="navlink2">Developers API</span>
             </Link>
           </li>
 
           {/* Support Section */}
-          <div className="menu_title font-semibold">Support</div>
+          <div className="menu_title font-semibold menu_support"></div>
           <li className="item">
             <Link to="/support/faqs" className={`nav_link ${isActive('/support/faqs')}`} title='FAQs'>
               <span className="navlink_icon">
                 <i className="pi pi-question-circle"></i>
               </span>
-              <span className="navlink">FAQs</span>
+              <span className="navlink2">FAQs</span>
             </Link>
           </li>
           <li className="item">
@@ -144,7 +152,7 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-envelope"></i>
               </span>
-              <span className="navlink">Contact Us</span>
+              <span className="navlink2">Contact Us</span>
             </Link>
           </li>
           <li className="item">
@@ -152,7 +160,7 @@ const SideBar = ({ sideBarClose, handleSideBarToggle }) => {
               <span className="navlink_icon">
                 <i className="pi pi-money-bill"></i>
               </span>
-              <span className="">Pricing</span>
+              <span className="navlink2">Pricing</span>
             </Link>
           </li>
 
