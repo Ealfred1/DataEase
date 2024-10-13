f7984aa032cddb1c0ae15025a33782c23e5ca2c2
Ealfred1 <alfrederic371@gmail.com>
2024-10-13 20:39:00 +0100

    addding navbar intercativity (style changes)

--- src/components/Auth/Navbar.jsx
+++ src/components/Auth/Navbar.jsx
@@ -22,31 +22,40 @@ const Navbar = ({ handleSideBarToggle, closed }) => {
   
   return (
     <nav className="navbar bg-transparent backdrop-blur-2xl dark:bg-slate-900 dark:border dark:border-slate-900 dark:border-b-gray-500">
-      <div className="logo-items">
-        <i className={`pi ${closed ? 'pi-times' : 'pi-bars'}`}  onClick={handleSideBarToggle}></i>
-        <h2 className="text-2xl">DataEase</h2>
-      </div>
-      
-      <div className={`search-bar ${display}`} onClick={() => handleNavigate()}>
-        <input type="text" className="dark:bg-transparent dark:border-gray-500 dark:text-white" placeholder="Search..." />
-      </div>
+  <div className="logo-items flex items-center">
+      <div className="menu-icon translate-y-1" onClick={handleSideBarToggle}>
+          <span></span>
+          <span></span>
+        </div>
+    <h2 className="text-2xl">DataEase</h2>
+  </div>
 
-      <div className="relative">
-        <img 
-          src={Image} 
-          alt="logo_img" 
-          className="w-[40px] h-[40px] rounded-full cursor-pointer object-cover ring ring-4 ring-offset-2 ring-vibrantGreen" 
-          onClick={() => setProfileMenuOpen(!profileMenuOpen)}
-        />
-        {profileMenuOpen && (
-          <div className="absolute right-0 mt-3 w-48 bg-white dark:bg-slate-800 rounded-md shadow-lg">
-            <Link to="/profile" className="block px-4 py-4 text-gray hover:bg-vibrantGreen rounded-t-md hover:text-white ">Profile</Link>
-            <Link to="/settings" className="block px-4 py-4 text-gray hover:bg-vibrantGreen hover:text-white ">Settings</Link>
-            <Link to="/logout" className="block px-4 py-4 text-gray  hover:text-white w-full hover:bg-vibrantGreen rounded-b-md hover:text-white ">Logout</Link>
-          </div>
-        )}
+  <div className="relative">
+    <img 
+      src={Image} 
+      alt="logo_img" 
+      className="w-[40px] h-[40px] rounded-full cursor-pointer object-cover ring ring-4 ring-offset-2 ring-vibrantGreen" 
+      onClick={() => setProfileMenuOpen(!profileMenuOpen)}
+    />
+    {profileMenuOpen && (
+      <div className="absolute right-0 mt-3 w-48 bg-white dark:bg-slate-800 rounded-md shadow-lg">
+        <Link to="/profile" className="flex items-center px-4 py-4 text-gray hover:bg-vibrantGreen rounded-t-md hover:text-white">
+          <i className="pi pi-user mr-2"></i>
+          Profile
+        </Link>
+        <Link to="/settings" className="flex items-center px-4 py-4 text-gray hover:bg-vibrantGreen hover:text-white">
+          <i className="pi pi-cog mr-2"></i>
+          Settings
+        </Link>
+        <Link to="/logout" className="flex items-center px-4 py-4 text-gray hover:text-white w-full hover:bg-vibrantGreen rounded-b-md hover:text-white">
+          <i className="pi pi-sign-out mr-2"></i>
+          Logout
+        </Link>
       </div>
-    </nav>
+    )}
+  </div>
+</nav>
+
   )
 }
 
