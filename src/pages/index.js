50b07dbcd242e2a2fa14d02e2dfe17bcfc851aa8
alfrederic371 <alfrederic371@gmail.com>
2024-10-23 14:49:00 +0000

    Chore: Exported All pages ready for external destrucuted import

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,19 @@ import LoginPage from './LoginPage'
 import RegisterPage from './RegisterPage'
 import DashboardPage from './DashboardPage'
 import SettingsPage from './SettingsPage'
+import ReferralsPage from './ReferralsPage'
+import AirtimePage from './AirtimePage'
+import APIPage from './APIPage'
+import BillsPage from './BillsPage'
+import BonusPage from './BonusPage'
+import CablePage from './CablePage'
+import ContactPage from './ContactPage'
+import ElectricityPage from './ElectricityPage'
+import FaqsPage from './FaqsPage'
+import FundAccoutPage from './FundAccoutPage'
+import PricingPage from './PricingPage'
+import TransactionsPaymentPage from './TransactionsPaymentPage'
+import TransactionsPurchase from './TransactionsPurchase'
 
 export {
 	LandingPage,
@@ -11,5 +24,18 @@ export {
 	RegisterPage,
 	LoginPage,
 	DashboardPage,
-	SettingsPage
+	SettingsPage,
+	ReferralsPage,
+	AirtimePage,
+	APIPage,
+	BillsPage,
+	BonusPage,
+	CablePage,
+	ContactPage,
+	ElectricityPage,
+	FaqsPage,
+	FundAccoutPage,
+	PricingPage,
+	TransactionsPaymentPage,
+	TransactionsPurchase
 }
